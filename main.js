bfadc887a5b2219c445717ccd86274ce46965e9a
Derrick Bell <dbell34@mail.valenciacollege.edu>
2021-02-14 22:11:20 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+ // Poblem 1
+ var myArray = [];
+ for (var i = 1000; i > 0; i -= 2) {
+    myArray.push(i);
+ }
+ 
+ console.log(myArray);
+  
+  // Problem 2 
+ 
+     for(let i = 0; i <= 100; i++) {
+      if(i == 25) {
+         alert("A quarter of the way there!");
+    } else if (i == 50) {
+       alert("Halfway there!");
+    } else (i == 100); {
+ 
+    }
+       
+ }
+ alert("The loop is done!");
+ 
+ 
+ 
+ // Problem 3 Favorite TV Shows
+ 
+ var tvShows = ["NCIS Los Angles", "Power", "Dateline NBC", "Matlock", "House Hunters"];
+ var myRankings = ["#1", "#2", "#3", "#4", "#5"]; 
+ 
+ for(let i = 0; i < tvShows.length; i++) {
+    console.log(tvShows[i]);
+ } 
+  if (myRankings [0] = tvShows [0]) {
+    console.log("My" +  "favorite TV show is" + tvShows[0]);   
+ }if (myRankings = tvShows [1]) {
+    console.log("My" + "favorite TV show is" + tvShows[1])
+ } if (myRankings = tvShows  [2]) {
+    console.log("My" + "favorite TV show is" + tvShows [2])
+ } if (myRankings = tvShows [3]) {
+    console.log("My" + "favorite TV show is" + tvShows [3])
+ } else (myRankings = tvShows [4] )
+    console.log("My" + "favorite TV show is" + tvShows [4])
+ 
+ 
